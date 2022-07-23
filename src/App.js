879db3caaa9fbbe15478a86bd5de06b482b77683
melaku <genuieman@gmail.com>
2022-07-23 16:22:19 +0300

    Programs Componet

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Hero from './component/Hero/Hero';
+import Programs from './component/Programs/Programs';
 function App() {
   return (
     <div className="App">
       <Hero />
+      <Programs />
     </div>
   );
 }
