e825287b3139e571b56efd78776d70c351f75dec
melaku <genuieman@gmail.com>
2022-07-23 18:41:21 +0300

    Reasons component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import Hero from './component/Hero/Hero';
 import Programs from './component/Programs/Programs';
+import Reasions from './component/Reasions/Reasions';
+
 function App() {
   return (
     <div className="App">
       <Hero />
       <Programs />
+      <Reasions />
     </div>
   );
 }
