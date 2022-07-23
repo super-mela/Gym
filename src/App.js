ec25e0911be5e41e78712479fc4fd69a310314ad
melaku <genuieman@gmail.com>
2022-07-23 20:34:52 +0300

    Plans Component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Hero from './component/Hero/Hero';
 import Programs from './component/Programs/Programs';
 import Reasions from './component/Reasions/Reasions';
+import Plans from './component/Palns/Plans';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Hero />
       <Programs />
       <Reasions />
+      <Plans />
     </div>
   );
 }
--- src/component/Programs/Programs.js
+++ src/component/Programs/Programs.js
@@ -19,7 +19,7 @@ const Programs = () => {
                         <span>{program.details}</span>
                         <div className='join-now'>
                             <span>Join Now</span>
-                            <img src={RightArrow} />
+                            <img src={RightArrow} alt='' />
                         </div>
                     </div>
                 ))}
