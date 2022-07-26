c149bf21fbc2b84d8546b12ffeb391c317c7935d
melaku <genuieman@gmail.com>
2022-07-26 14:15:48 +0300

    number counter

--- src/component/Hero/Hero.js
+++ src/component/Hero/Hero.js
@@ -6,6 +6,7 @@ import hero_image_back from '../../assets/hero_image_back.png';
 import Heart from '../../assets/heart.png';
 import Calories from '../../assets/calories.png';
 import { motion } from 'framer-motion';
+import NumberCounter from 'number-counter'
 
 const Hero = () => {
 
@@ -19,7 +20,7 @@ const Hero = () => {
                 <Header />
                 <div className='the-best-ad'>
                     <motion.div
-                        initial={{ left: mobile ? '165px' : '238px' }}
+                        initial={{ left: mobile ? '165 px' : '238px' }}
                         whileInView={{ left: '8px' }}
                         transition={{ ...transition, type: "tween" }}
                     ></motion.div>
@@ -43,14 +44,20 @@ const Hero = () => {
                 </div>
                 <div className='figures'>
                     <div>
-                        <span>+140</span>
+                        <span>
+                            <NumberCounter end={140} start={100} delay='4' priFix="+" />
+                        </span>
                         <span>expert coachs</span>
                     </div>
                     <div>
-                        <span>+978</span>
+                        <span>
+                            <NumberCounter end={978} start={500} delay='4' priFix="+" />
+                        </span>
                         <span>members joined</span>
                     </div> <div>
-                        <span>+50</span>
+                        <span>
+                            <NumberCounter end={50} start={10} delay='4' priFix="+" />
+                        </span>
                         <span>fitness programs</span>
                     </div>
                 </div>
