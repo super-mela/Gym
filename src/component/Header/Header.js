f315026a7f6961b177a5a69a6ac1beda38563afc
melaku <genuieman@gmail.com>
2022-07-26 12:28:41 +0300

    Make Responsive

--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -1,18 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Header.css'
 import Logo from '../../assets/logo.png'
+import Bars from '../../assets/bars.png'
 
 export const Header = () => {
+
+    const mobile = window.innerWidth <= 768 ? true : false;
+    const [menuOpen, setMenuOpen] = useState(false)
+
     return (
         <div className='header'>
             <img src={Logo} alt='Logo' className='logo' />
-            <ul className='header-menu'>
-                <li>Home</li>
-                <li>Programs</li>
-                <li>Why us</li>
-                <li>Plans</li>
-                <li>Testimonals</li>
-            </ul>
+            {(menuOpen === false && mobile === true) ?
+                <div style={{
+                    backgroundColor: "var(--appColor)",
+                    padding: "0.5rem",
+                    borderRadius: "5px"
+                }}
+                    onClick={() => setMenuOpen(true)}
+                >
+                    <img src={Bars} alt='' style={{ width: "1.5rem", height: "1.5rem" }} />
+                </div> :
+                <ul className='header-menu'>
+                    <li onClick={() => setMenuOpen(false)}>Home</li>
+                    <li onClick={() => setMenuOpen(false)}>Programs</li>
+                    <li onClick={() => setMenuOpen(false)}>Why us</li>
+                    <li onClick={() => setMenuOpen(false)}>Plans</li>
+                    <li onClick={() => setMenuOpen(false)}>Testimonals</li>
+                </ul>
+            }
+
         </div>
     )
 }
